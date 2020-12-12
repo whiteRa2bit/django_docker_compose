ff0782c9c5c8d7c8dd1613277dc9b095a17decba
whiteRa2bit <pavel.fakanov@gmail.com>
2020-12-12 10:09:42 +0300

    Upd tests

--- src/tests/test_server.py
+++ src/tests/test_server.py
@@ -9,7 +9,8 @@ if SERVER_PORT != 80:
     URL += ':{}'.format(SERVER_PORT)
 
 
-def test_hello_world():
-    response = requests.get(URL + '/')
-    assert response.text == 'Hello, World!'
+def test_list_items():
+    url = URL + '/items'
+    response = requests.get(url)
+    print(response.content)
     assert response.status_code == 200
