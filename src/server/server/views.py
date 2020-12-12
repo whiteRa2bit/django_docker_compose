ce825f2273d2fdefdf16c217a8ceabf96c36b216
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-12-12 17:58:02 +0000

    Tests

--- src/server/server/views.py
+++ src/server/server/views.py
@@ -1,7 +1,8 @@
+import json
+
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
-import json
 
 from .models import Item
 from .config import ITEM_FIELDS
