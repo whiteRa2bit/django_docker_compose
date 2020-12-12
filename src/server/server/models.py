0cf02da22743b00acac732621f3d60d43e44f696
whiteRa2bit <pavel.fakanov@gmail.com>
2020-12-12 10:05:23 +0300

    Initial commit

---
+++ src/server/server/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from .config import ITEM_FIELDS
+
+# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    topic = models.CharField(max_length=200)
+    rate = models.IntegerField()
+    link = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def to_dict(self):
+        return {field: getattr(self, field) for field in ITEM_FIELDS}
---
+++ src/server/server/views.py
@@ -0,0 +1,35 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+
+from .models import Item
+from .config import ITEM_FIELDS
+
+
+def _success(data=None):
+    response_dict = {'ok': True, 'errors': {}}
+    if data is not None:
+        response_dict['data'] = data
+    return JsonResponse(response_dict)
+
+
+def _error(errors, status=400):
+    return JsonResponse({'ok': False, 'errors': errors}, status=status)
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def add_item(request):
+    params = json.loads(request.body.decode())
+    item_fields = {field: params.get(field, '') for field in ITEM_FIELDS}
+    item = Item(**item_fields)
+    item.save()
+
+    return _success({'item': item.to_dict()})
+
+
+@require_http_methods(['GET'])
+def get_items(request):
+    items = Item.objects.all()
+    return _success({'items': [item.to_dict() for item in items]})
