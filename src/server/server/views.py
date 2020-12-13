516527702f70fd0d22b4a2c28ff73deae933aa03
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-12-14 01:49:38 +0800

    Tests (#2)
    * Add run script
    
    * Add django app
    
    * add run_docker.sh for server
    
    * Upd Dockerfile
    
    * Upd requirements
    
    * Delete flask app
    
    * Upd tests
    
    * add new endpoints
    
    * New endpoints
    
    * Add clean db script
    
    * Clean db in dockerfile
    
    * migrations
    
    * Add tests
    
    * Replace 1 with ITEM_ID
    
    * Upd dockerfile
    
    * Prettify
    
    * prettify
    
    * port 80
    
    * Merge dockerfile

--- src/server/server/views.py
+++ src/server/server/views.py
@@ -1,7 +1,8 @@
+import json
+
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
-import json
 
 from .models import Item
 from .config import ITEM_FIELDS
@@ -43,7 +44,8 @@ def get_item(request, id):
 @csrf_exempt
 @require_http_methods(['POST'])
 def add_item(request):
-    params = json.loads(request.body.decode())    is_valid = _validate_fields(params)
+    params = json.loads(request.body.decode())
+    is_valid = _validate_fields(params)
     if is_valid:
         item_fields = {field: params.get(field) for field in ITEM_FIELDS}
         item = Item(**item_fields)
